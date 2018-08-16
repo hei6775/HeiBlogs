14c48983800411691fe49347ae41b952fb4c8aa1
hei6775 <30398214+hei6775@users.noreply.github.com>
2018-08-16 18:22:52 +0800

    Add files via upload

---
+++ lecture01/Binary_Search.go
@@ -0,0 +1,34 @@
+package lecture01
+
+
+func Binary_Search(a []int,tar int)(int){
+	left := 0
+	right := len(a)
+	for {
+		n := (left+right)/2
+		if tar > a[n]{
+			left = n+1
+		}else if tar < a[n]{
+			right = n
+		}else{
+			return n
+		}
+	}
+	return -1
+}
+
+
+//func main()  {
+//	a := []int{1,2,3,4,5,6,7,8,9}
+//	fmt.Println("len n :",len(a))
+//	result := Binary_Search(a,5)
+//	fmt.Println("result:",result)
+//	result = Binary_Search(a,3)
+//	fmt.Println("result:",result)
+//	result = Binary_Search(a,7)
+//	fmt.Println("result:",result)
+//	result = Binary_Search(a,1)
+//	fmt.Println("result:",result)
+//	result = Binary_Search(a,9)
+//	fmt.Println("result:",result)
+//}
