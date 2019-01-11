fc70b45e005d954b0af54c63dd0eb7342276b14f
hei6775 <913799761@qq.com>
2019-01-11 18:01:56 +0800

    二分法查找新增一种实现

--- lecture01/Binary_Search.go
+++ lecture01/Binary_Search.go
@@ -1,5 +1,10 @@
 package lecture01
 
+import (
+	"sort"
+	"fmt"
+)
+
 //二分法
 
 func Binary_Search(a []int,tar int)(int){
@@ -18,7 +23,38 @@ func Binary_Search(a []int,tar int)(int){
 	return -1
 }
 
+//第二种实现方法
+type BBQ struct {
+	v int
+}
 
+type BBQS []*BBQ
+
+func (self BBQS) Len() int           { return len(self) }
+func (self BBQS) Swap(i, j int)      { self[i], self[j] = self[j], self[i] }
+func (self BBQS) Less(i, j int) bool { return self[i].v < self[j].v }
+func (self BBQS) Sort()              { sort.Sort(self) }
+func (self BBQS) Search(v int) int {
+	return sort.Search(len(self), func(i int) bool {
+		fmt.Printf("self index[%v] data [%v] \n", i, self[i])
+		return self[i].v <= v
+	})
+}
+func (self BBQS) Search2(v int) int {
+	left := 0
+	right := len(self)
+	for n := (left + right) / 2; left < right; n = (left + right) / 2 {
+		fmt.Printf("index [%v] data [%v] \n", n, self[n])
+		if self[n].v < v {
+			left = n + 1
+		} else if self[n].v > v {
+			right = n
+		} else {
+			return n
+		}
+	}
+	return -1
+}
 //func main()  {
 //	a := []int{1,2,3,4,5,6,7,8,9}
 //	fmt.Println("len n :",len(a))
