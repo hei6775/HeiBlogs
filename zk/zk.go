eda5cdf31edadcc7b92e9b9a65524e2a872200c3
hei6775 <913799761@qq.com>
2019-01-28 19:14:58 +0800

    update zk moudle
    Signed-off-by: hei6775 <913799761@qq.com>

--- zk/zk.go
+++ zk/zk.go
@@ -7,19 +7,55 @@ import (
 	"time"
 )
 
+const (
+	TypeCreateNodeLasting     = iota //持久节点
+	TypeCreateNodeEphemeral          //暂态节点
+	TypeCreateNodeLasSequence        //持久有序节点
+	TypeCreateNodeEphSequence        //暂态有序节点
+	TypeDeleteNode                   //删除节点4
+	TypeSetNodeDate                  //修改节点数据5
+	TypeGetNodeData                  //获取节点数据6
+	TypeExistNode                    //节点存在与否7
+	TypeGetChilds                    //节点存在与否7
+)
+
+const (
+	PermRead = 1 << iota
+	PermWrite
+	PermCreate
+	PermDelete
+	PermAdmin
+	PermAll = 0x1f
+)
+
 type ZK struct {
 	Conn      *zk.Conn
 	Servers   []string `json:"servers"`
 	Timeout   int      `josn:"timeout"`
-	MsgChan   chan *RetChan
-	functions map[interface{}]interface{}
+	MsgChan   chan *MsgChan
+	eChan     <-chan zk.Event
+	functions map[interface{}]Funcs
 }
 
-type RetChan struct {
-	id    interface{}
-	etype int
-	args  []interface{} //参数
-	cb    interface{}   //回调函数
+type Funcs interface {
+	DealWith([]byte, string) error
+	OnDestry()
+}
+
+type MsgChan struct {
+	Type interface{}
+	Id   interface{}
+	Args []interface{} //参数
+	Cb   interface{}   //回调函数
+}
+
+//
+type CallArg struct {
+	Id    interface{}
+	State interface{}
+	Data  interface{}
+	Perm  interface{}
+	Error error
 }
 
 func NewGoZk(config string) (*ZK, error) {
@@ -30,9 +66,9 @@ func NewGoZk(config string) (*ZK, error) {
 		return nil, fmt.Errorf("config [%v]", err)
 	}
 	if gozk.Timeout > 0 {
-		gozk.Conn, _, err = zk.Connect(gozk.Servers, time.Duration(gozk.Timeout)*time.Second)
+		gozk.Conn, gozk.eChan, err = zk.Connect(gozk.Servers, time.Duration(gozk.Timeout)*time.Second)
 	} else {
-		gozk.Conn, _, err = zk.Connect(gozk.Servers, 10*time.Second)
+		gozk.Conn, gozk.eChan, err = zk.Connect(gozk.Servers, 10*time.Second)
 	}
 	if err != nil {
 		return nil, fmt.Errorf("Connnect invalid [%v]", err)
@@ -40,55 +76,143 @@ func NewGoZk(config string) (*ZK, error) {
 	return gozk, nil
 }
 
-func (this *ZK) Register(id interface{}, f interface{}) {
-	switch f.(type) {
-	case func([]interface{}):
-	case func([]interface{}) interface{}:
-	case func([]interface{}) []interface{}:
-	default:
-		panic(fmt.Sprintf("function id %v:definition of function is invalid", id))
-	}
-
+func (this *ZK) Register(id interface{}, funcs Funcs) {
 	if _, ok := this.functions[id]; ok {
-		panic(fmt.Sprintf("function is %v: already registered"))
+		panic(fmt.Sprintf("Funcs interface is %v: already registered"))
 	}
 
-	this.functions[id] = f
+	this.functions[id] = funcs
 }
 
-func (this *ZK) execute(ci *RetChan) (err error) {
+func (this *ZK) Run() {
+	for {
+		select {
+		case eve := <-this.eChan:
+			if eve.Type == zk.EventNodeDataChanged {
+				this.resetWatch(eve.Path)
+			}
+			if eve.State == zk.StateDisconnected {
+				this.stop()
+				break
+			}
+		}
+	}
+}
+
+func (this *ZK) sendChan(ci *MsgChan) {
+	this.MsgChan <- ci
+}
+
+func (this *ZK) setWatch() {
+	for k, f := range this.functions {
+		path := k.(string)
+		data, _, _, err := this.Conn.GetW(path)
+		if err != nil {
+			panic(fmt.Sprintf("path[%v] err[%v]", path, err))
+		}
+
+		if f == nil {
+			err = fmt.Errorf("function id %v: function not registered")
+			return
+		}
+		err = f.DealWith(data, path)
+		panic(err)
+	}
+}
+
+func (this *ZK) resetWatch(path string) error {
+	data, _, _, err := this.Conn.GetW(path)
+	if err != nil {
+		return err
+	}
+
+	f := this.functions[path]
+	if f == nil {
+		err = fmt.Errorf("function id %v: function not registered", path)
+		return err
+	}
+	err = f.DealWith(data, path)
+	return err
+
+}
+
+func (this *ZK) stop() {
+
+}
+
+func (this *ZK) execute(ci *MsgChan) (err error) {
 	defer func() {
 		if r := recover(); r != nil {
 			fmt.Errorf("catch panic %v", r)
 		}
 	}()
 
-	f := this.functions[ci.id]
-	if f == nil {
-		err = fmt.Errorf("function id %v: function not registered", ci.id)
-		return
+	switch ci.Type {
+	case TypeCreateNodeLasting:
+	case TypeCreateNodeEphemeral:
+	case TypeCreateNodeLasSequence:
+	case TypeCreateNodeEphSequence:
+	case TypeDeleteNode:
+	case TypeSetNodeDate:
+	case TypeGetNodeData:
+	case TypeExistNode:
+	default:
+		err = fmt.Errorf("invalid ci type: %v", ci.Type)
+		return err
 	}
+	callArg := new(CallArg)
+	args := ci.Args
+	tarArgs := []interface{}{
+		callArg,
+	}
+	tarArgs = append(tarArgs, args...)
+	this.callBack(ci.Cb, tarArgs)
 
-	switch f.(type) {
-	case func([]interface{}):
-		f.(func([]interface{}))(ci.args)
-	case func([]interface{}) interface{}:
-		ret := f.(func([]interface{}) interface{})(ci.args)
-		fmt.Println(ret)
-	case func([]interface{}) []interface{}:
-		ret := f.(func([]interface{}) []interface{})(ci.args)
-		fmt.Println(ret)
-	}
 	return nil
 }
 
-func (this *ZK) sendChan(ci *RetChan) {
-	this.MsgChan <- ci
+func (this *ZK) callBack(cb interface{}, args []interface{}) {
+	//callBack
+	switch cb.(type) {
+	case func([]interface{}):
+		cb.(func([]interface{}))(args)
+	case func([]interface{}) interface{}:
+		_ = cb.(func([]interface{}) interface{})(args)
+	case func([]interface{}) []interface{}:
+		_ = cb.(func([]interface{}) []interface{})(args)
+	}
+	return
 }
 
-func (this *ZK) setWatch() {
-	for _, v := range this.functions {
-		path := v.(string)
+func (this *ZK) createNodes(data, path, flag, perm interface{}) error {
+	this.Conn.Create()
+	return nil
+}
 
-	}
+func (this *ZK) deleteNode(data, path interface{}) error {
+	this.Conn.Delete()
+	return nil
+}
+
+func (this *ZK) existNode(data, path interface{}) error {
+	this.Conn.Exists()
+	return nil
+}
+
+func (this *ZK) getNodeData(data, path interface{}) error {
+	this.Conn.Get()
+	return nil
+}
+
+func (this *ZK) setNodeData(data, path interface{}) error {
+	tardata := data.([]byte)
+	tarpath := path.(string)
+	_, err := this.Conn.Set(tarpath, tardata, -1)
+	return err
+}
+
+func (this *ZK) getchild(path interface{}) ([]string, error) {
+	tarpath := path.(string)
+	childrens, _, err := this.Conn.Children(tarpath)
+	return childrens, err
 }
