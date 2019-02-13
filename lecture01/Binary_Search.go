966e8838096511db54aeeebd4b4d609bfe13275d
hei6775 <913799761@qq.com>
2019-02-13 11:20:32 +0800

    add the test file to check u think of the code
    Signed-off-by: hei6775 <913799761@qq.com>

--- lecture01/Binary_Search.go
+++ lecture01/Binary_Search.go
@@ -1,22 +1,22 @@
 package lecture01
 
 import (
-	"sort"
 	"fmt"
+	"sort"
 )
 
 //二分法
 
-func Binary_Search(a []int,tar int)(int){
+func Binary_Search(a []int, tar int) int {
 	left := 0
 	right := len(a)
 	for {
-		n := (left+right)/2
-		if tar > a[n]{
-			left = n+1
-		}else if tar < a[n]{
+		n := (left + right) / 2
+		if tar > a[n] {
+			left = n + 1
+		} else if tar < a[n] {
 			right = n
-		}else{
+		} else {
 			return n
 		}
 	}
@@ -34,6 +34,8 @@ func (self BBQS) Len() int           { return len(self) }
 func (self BBQS) Swap(i, j int)      { self[i], self[j] = self[j], self[i] }
 func (self BBQS) Less(i, j int) bool { return self[i].v < self[j].v }
 func (self BBQS) Sort()              { sort.Sort(self) }
+
+//适合从大到小排序，寻找符合条件的最小的index
 func (self BBQS) Search(v int) int {
 	return sort.Search(len(self), func(i int) bool {
 		fmt.Printf("self index[%v] data [%v] \n", i, self[i])
@@ -55,17 +57,3 @@ func (self BBQS) Search2(v int) int {
 	}
 	return -1
 }
-//func main()  {
-//	a := []int{1,2,3,4,5,6,7,8,9}
-//	fmt.Println("len n :",len(a))
-//	result := Binary_Search(a,5)
-//	fmt.Println("result:",result)
-//	result = Binary_Search(a,3)
-//	fmt.Println("result:",result)
-//	result = Binary_Search(a,7)
-//	fmt.Println("result:",result)
-//	result = Binary_Search(a,1)
-//	fmt.Println("result:",result)
-//	result = Binary_Search(a,9)
-//	fmt.Println("result:",result)
-//}
--- lecture01/Quick_Find.go
+++ lecture01/Quick_Find.go
@@ -6,29 +6,24 @@ type Obj struct {
 	intid []int
 }
 
-func (O *Obj)Init(N int){
-	for i:=0; i<N; i++{
-		O.intid= append(O.intid,i)
+func (O *Obj) Init(N int) {
+	for i := 0; i < N; i++ {
+		O.intid = append(O.intid, i)
 	}
 }
-func (O *Obj)Connect(p,q int)(bool){
+
+//判断是否连通
+func (O *Obj) Connect(p, q int) bool {
 	return O.intid[p] == O.intid[q]
 }
 
-func (O *Obj)Union(p,q int){
+//连通两个节点
+func (O *Obj) Union(p, q int) {
 	pid := O.intid[p]
 	qid := O.intid[q]
-	for i:=0;i<len(O.intid);i++{
-		if (O.intid[i] == pid){
+	for i := 0; i < len(O.intid); i++ {
+		if O.intid[i] == pid {
 			O.intid[i] = qid
 		}
 	}
 }
-
-//func main(){
-//	O := new(Obj)
-//	O.Init(10)
-//	O.Connect(0,2)
-//	O.Union(0,2)
-//	fmt.Println(O)
-//}
