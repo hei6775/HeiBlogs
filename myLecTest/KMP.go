2a503dce5dfb136c2b16642d60f87550d50b146a
hei6775 <913799761@qq.com>
2019-02-24 19:22:24 +0800

    update
    Signed-off-by: hei6775 <913799761@qq.com>

--- myLecTest/KMP.go
+++ myLecTest/KMP.go
@@ -7,16 +7,19 @@ package myLecTest
 //}
 
 func getNext(p string) []int {
-	l := len(p)
-	t := make([]int, l)
+	l := len(p) //模式字符串的长度
+	t := make([]int, l)//返回最长前缀后缀数组，初始化都为0
 	t[0] = 0
-	i, j := 0, 1
+	i, j := 0, 1//i用来记录长度，j是p的当前位置index
 	for j < l {
+		//如果相等 赋值
 		if p[i] == p[j] {
 			t[j] = i + 1
 			i++
 			j++
 		} else {
+			//当p[i],p[j]不相等时
+			//当i不等于0的时候取t[i-1]的值 否则t[j]=0
 			if i != 0 {
 				i = t[i-1]
 			} else {
