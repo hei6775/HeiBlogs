d88addd51da8c8081987859c04f3c326a99ebcdc
hei6775 <913799761@qq.com>
2019-04-22 23:00:18 +0800

    update the array delay
    Signed-off-by: hei6775 <913799761@qq.com>

---
+++ Until/common/common.go
@@ -0,0 +1,16 @@
+package common
+
+//space O(1)
+func RotateSlcesInt(nums []int,k int){
+	n := len(nums) //数组长度
+	k %= n //如果k>n的情况，则取k/n的余数
+
+}
+
+func reverse(nums []int,start,end int){
+	for start < end {
+		nums[start], nums[end] = nums[end], nums[start]
+		start++
+		end--
+	}
+}
